2ed40a478a6915f8cdf7e01581afc77caf7e4cbf
RJrijo123 <rijojacob2001@gmail.com>
2024-06-15 18:18:07 +0530

    commit

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './BookList.css';
+import './BookList.css';  
 
 const BookList = ({ onEdit, onDelete }) => {
   const [books, setBooks] = useState([]);
